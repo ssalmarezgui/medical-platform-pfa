687e5e49b59cc93106e3ab27dff9eb491d1fb716
ssalma rezgui <ssalma.rezgui@ensi-uma.tn>
2026-07-13 13:50:50 +0100

    iac: integrer la validation de l'architecture AWS avec Terraform et LocalStack

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "mock_key"
+  secret_key                  = "mock_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Mon-Serveur-Web-AWS-Simule"
+  }
+}
