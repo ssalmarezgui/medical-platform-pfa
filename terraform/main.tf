1c0ab76f28071e72d46e5c1c881409ca905bce54
ssalma rezgui <ssalma.rezgui@ensi-uma.tn>
2026-07-13 21:17:21 +0100

    sec: corriger les failles d'IaC de l'EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,25 @@ resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
+  # Correction de CKV_AWS_126 : Activer la surveillance détaillée CloudWatch
+  
+  monitoring = true 
+
+  # Correction de CKV_AWS_8 : Chiffrer le disque dur principal (Root Volume)
+
+  root_block_device {
+    encrypted = true
+  }
+
+
+  # Correction de CKV_AWS_79 : Forcer l'usage d'IMDSv2 (Désactive la V1 vulnérable)
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
   tags = {
     Name = "Mon-Serveur-Web-AWS-Simule"
   }
